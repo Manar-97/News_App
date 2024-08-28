d9d7ee8834c767428b472601f718b9519a14ad25
Manar3teto <Manar-97@github.com>
2024-08-28 19:21:46 +0300

    Applied MVVM pattern with provider

--- lib/UI/screens/home/tabs/tabs_list_tab/tabs_list.dart
+++ lib/UI/screens/home/tabs/tabs_list_tab/tabs_list.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/UI/screens/home/tabs/tabs_list_tab/tabs_view_model.dart';
 import 'package:news_app/UI/widgets/error_view.dart';
 import 'package:news_app/UI/widgets/loading_view.dart';
+import 'package:provider/provider.dart';
 import '../../../../../data/api/api_manager.dart';
 import '../../../../../data/model/source.dart';
 import 'news_list.dart';
 
 class TabsList extends StatefulWidget {
   final String categoryId;
-  const TabsList(this.categoryId,{super.key});
+  const TabsList(this.categoryId, {super.key});
 
   @override
   State<TabsList> createState() => _TabsListState();
@@ -15,20 +17,30 @@ class TabsList extends StatefulWidget {
 
 class _TabsListState extends State<TabsList> {
   int selectedTabIndex = 0;
+  TabsViewModel viewModel = TabsViewModel();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    viewModel.getSources(widget.categoryId);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: ApiManager.getSources(widget.categoryId),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return ErrorView(
-              error: snapshot.error.toString(), onRetryClick: () {});
-        } else if (snapshot.hasData) {
-          return buildTabsList(snapshot.data!.sources!);
-        } else {
-          return const LoadingView();
-        }
+    return ChangeNotifierProvider(
+      create: (_) => viewModel,
+      builder: (context, _) {
+        viewModel = Provider.of(context);
+       if(viewModel.sourcesApiState is BaseLoadingState){
+         return const LoadingView();
+       }else if(viewModel.sourcesApiState is BaseErrorState){
+         String errorMessage = (viewModel.sourcesApiState as BaseErrorState).errorMessage;
+         return ErrorView(error: errorMessage, onRetryClick: (){});
+       }else{
+         List<Source> sources = (viewModel.sourcesApiState as BaseSuccessState<List<Source>>).data;
+         return buildTabsList(sources);
+       }
       },
     );
   }
@@ -36,10 +48,10 @@ class _TabsListState extends State<TabsList> {
   Widget buildTabsList(List<Source> sources) {
     List<Widget> tabs = sources
         .map((source) =>
-        mapSourceToTab(source, selectedTabIndex == sources.indexOf(source)))
+            mapSourceToTab(source, selectedTabIndex == sources.indexOf(source)))
         .toList();
     List<Widget> tabsBody =
-    sources.map((source) => NewsList(source: source)).toList();
+        sources.map((source) => NewsList(source: source)).toList();
     return DefaultTabController(
       length: sources.length,
       child: Column(
@@ -77,3 +89,17 @@ class _TabsListState extends State<TabsList> {
     );
   }
 }
+
+///    return FutureBuilder(
+//       future: TabsViewModel,
+//       builder: (context, snapshot) {
+//         if (snapshot.hasError) {
+//           return ErrorView(
+//               error: snapshot.error.toString(), onRetryClick: () {});
+//         } else if (snapshot.hasData) {
+//           return buildTabsList(snapshot.data!.sources!);
+//         } else {
+//           return const LoadingView();
+//         }
+//       },
+//     );
--- lib/UI/screens/home/tabs/tabs_list_tab/tabs_view_model.dart
+++ lib/UI/screens/home/tabs/tabs_list_tab/tabs_view_model.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/data/model/source.dart';
+
+import '../../../../../data/api/api_manager.dart';
+import '../../../../base/base_api_state.dart';
 
 class TabsViewModel extends ChangeNotifier {
-
+  late BaseApiState sourcesApiState;
+  getSources(String categoryId) async {
+    try {
+      sourcesApiState = BaseLoadingState();
+      notifyListeners();
+      List<Source> sources = (await ApiManager.getSources(categoryId)).sources!;
+      sourcesApiState = BaseSuccessState<List<Source>>(sources);
+      notifyListeners();
+    } catch (e) {
+      sourcesApiState = BaseErrorState(e.toString());
+      notifyListeners();
+    }
+  }
 }
